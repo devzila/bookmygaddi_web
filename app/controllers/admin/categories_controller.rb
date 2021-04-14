d67cc5e8779795f4412a8d9ab602af6d11535204
DEVZILA-LAPI1 <priyanka@devzila.com>
2021-04-14 13:29:37 +0530

    correct the new and edit

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CategoriesController < Admin::BaseController
-  before_action :set_category, only: %i(show edit)
+  before_action :set_category, only: %i(show edit crea)
   def index
     @categories = Content.categories
   end
@@ -14,32 +14,30 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def create
-      @category.new({
-        title: params[:title],
-        slug: params[:slug]
-      })
-
-      redirect_to admin_category_path(@category.Content)
+    @category = Content.new(params.require(:categories).permit(:title, :slug))
+    @category.save
+    redirect_to admin_categories_path
   end
 
   def edit
 
   end
 
+
   def update
-    @category.update({
-      title: params[:title],
-      slug: params[:slug]
-    })
+  @category = Content.find(params[:id])
+    if @category.update(params.require(:content).permit(:title, :slug))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to admin_categories_path
 
-    redirect_to admin_category_path
+    end
   end
-
 
   private
 
   def set_category
     @category = Content.find(params[:id])
   end
+
 
 end
