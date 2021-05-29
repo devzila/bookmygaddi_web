6ef5b203e37a759cdc6d7f48011fee6ebf3c1c19
Nilay Anand <nilayanand@gmail.com>
2021-05-29 18:30:48 +0530

    Add device login for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin
+
   resources :receiver, path: 'form-receiver', only: %i(new create)
   resources :home, :path => '/', only: %i(index show) do
     resources :articles, path: 'hire', only: %i(show)
