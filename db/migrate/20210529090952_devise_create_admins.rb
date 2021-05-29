8c8d3757110dd93a6406e83af50111fc7ae2b136
Nilay Anand <nilayanand@gmail.com>
2021-05-29 18:48:43 +0530

    Fix deploy

--- db/migrate/20210529090952_devise_create_admins.rb
+++ db/migrate/20210529090952_devise_create_admins.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
   def change
-    create_table :devise do |t|
+    create_table :admins do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
