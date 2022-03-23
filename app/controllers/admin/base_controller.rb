3cb0c1c1bcd45cfe5299b4193b4553d74b0078f4
Nilay Anand <nilayanand@gmail.com>
2022-03-23 21:54:14 +0530

    fix

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,6 +2,8 @@ class Admin::BaseController < ApplicationController
   before_action :authenticate_admin!
   layout "admin/application"
 
+  skip_before_action :verify_authenticity_token
+
 
 
   def after_sign_out_path_for(resource_or_scope)
