1b4aeeba864595edab0a075c04d665abf2bdb505
Nilay Anand <nilayanand@gmail.com>
2022-03-27 16:01:13 +0530

    fix

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,5 @@
 class Admin::BaseController < ApplicationController
-  before_action :authenticate_admin!
+  # before_action :authenticate_admin!
   skip_before_action :verify_authenticity_token
 
   layout "admin/application"
